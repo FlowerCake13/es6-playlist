6b02fef0d47b9a99dd1b311a341a062dc6b619bc
Lindsay M <lminami23@punahou.edu>
2018-09-29 11:00:28 -1000

    Add files via upload

---
+++ js/data.js
@@ -0,0 +1,18 @@
+var data = [{
+	artist: "Lauv and Julia Michaels",
+	genre: "Pop",
+	song: "There's No Way",
+	image: "../images/no-way.jpg"
+}, {
+	artist: "Bennny Blanco, Halsey, Khalid",
+	genre: "Pop",
+	song: "Eastside",
+	image: "../images/eastside"
+}, {
+	artist: "Pop"
+}, {
+	artist: "XXTENTACION",
+	genre: "Ballad",
+	song: "changes",
+	image: "../images/changes"
+}]
