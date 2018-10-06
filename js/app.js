9e5312a2ccdfbb5526864d34599be5a87076da00
Lindsay M <lminami23@punahou.edu>
2018-10-06 11:01:34 -1000

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -1,1 +1,31 @@
-console.log('test')
+console.log(data)
+
+var pop = data.filter(function (data){
+	return data.genre == "Pop"
+})
+console.log(pop)
+
+var hiphop = data.filter(function (data){
+	return data.genre == "Hip-Hop"
+})
+console.log(hiphop)
+
+var electronic = data.filter(function (data){
+	return data.genre == "Electronic"
+})
+console.log(electronic)
+
+var alternative = data.filter(function (data){
+	return data.genre == "Alternative"
+})
+console.log(alternative)
+
+var rb = data.filter(function (data){
+	return data.genre == "R&B"
+})
+console.log(rb)
+
+var country = data.filter(function (data){
+	return data.genre == "Country"
+})
+console.log(country)
--- js/data.js
+++ js/data.js
@@ -7,12 +7,170 @@ var data = [{
 	artist: "Bennny Blanco, Halsey, Khalid",
 	genre: "Pop",
 	song: "Eastside",
-	image: "../images/eastside"
+	image: "../images/eastside.jpg"
 }, {
-	artist: "Pop"
+	artist: "Quinn XCII, Chelsea Cutler",
+	genre: "Pop",
+	song: "Flare Guns",
+	image: "../images/flare-guns.jpg"
 }, {
-	artist: "XXTENTACION",
-	genre: "Ballad",
-	song: "changes",
-	image: "../images/changes"
+	artist: "Lauv",
+	genre: "Pop",
+	song: "Getting Over You",
+	image: "../images/getting-over.jpg"
+}, {
+	artist: "Billy Joel",
+	genre: "Pop",
+	song: "Uptown Girl",
+	image: "../images/uptown-girl.jpeg"
+}, {
+	artist: "gnash, Olivia Obrien",
+	genre: "Pop",
+	song: "I hate you, I love you",
+	image: "../images/hate-love.jpg"
+}, {
+	artist: "Lil Peep, XXXTentacion",
+	genre: "Hip-Hop",
+	song: "Falling Down",
+	image: "../images/falling-down.jpg"
+}, {
+	artist: "Rich The Kid",
+	genre: "Hip-Hop",
+	song: "Plug Walk",
+	image: "../images/plug-walk.jpg"
+}, {
+	artist: "Lil Uzi Vert",
+	genre: "Hip-Hop",
+	song: "XO Tour Lif3",
+	image: "../images/xo-tour-life.jpg"
+}, {
+	artist: "XXXTentacion",
+	genre: "Hip-Hop",
+	song: "Jocelyn Flores",
+	image: "../images/jocylen-flores.jpg"
+}, {
+	artist: "Juicde WRLD",
+	genre: "Hip-Hop",
+	song: "Lucid Dreams",
+	image: "../images/lucid-dreams.jpg"
+}, {
+	artist: "Post Malone",
+	genre: "Hip-Hop",
+	song: "I Fall Apart",
+	image: "../images/fall-apart.jpg"
+}, {
+	artist: "Zedd, Maren Morris",
+	genre: "Electronic",
+	song: "The Middle",
+	image: ".../images/"
+}, {
+	artist: "Marshmello, Khalid",
+	genre: "Electronic",
+	song: "Silence",
+	image: ".../images/"
+}, {
+	artist: "Chainsmokers, Kelsea Barllerini",
+	genre: "Electronic",
+	song: "This Feeling",
+	image: ".../images/"
+}, {
+	artist: "Alan Walker, Noah Cyrus, Digital Farm Animals, Juliander",
+	genre: "Electronic",
+	song: "All Falls Down",
+	image: ".../images/"
+}, {
+	artist: "Icona Pop",
+	genre: "Electronic",
+	song: "I Love It",
+	image: ".../images/"
+}, {
+	artist: "David Guetta, Sia",
+	genre: "Electronic",
+	song: "Titanium",
+	image: ".../images/"
+}, {
+	artist: "Against the Current",
+	genre: "Alternative",
+	song: "Strangers Again",
+	image: ".../images/"
+}, {
+	artist: "Against The Current",
+	genre: "Alternative",
+	song: "Sweet Surrender",
+	image: ".../images/"
+}, {
+	artist: "Imagine Dragons",
+	genre: "Alternative",
+	song: "Natural",
+	image: ".../images/"
+}, {
+	artist: "Dennis Lloyd",
+	genre: "Alternative",
+	song: "Nevermind",
+	image: ".../images/"
+}, {
+	artist: "The Postal Service",
+	genre: "Alternative",
+	song: "Such Great Heights",
+	image: ".../images/"
+}, {
+	artist: "AJR",
+	genre: "Alternative",
+	song: "Burn the House Down",
+	image: ".../images/"
+}, {
+	artist: "",
+	genre: "R&B",
+	song: "",
+	image: ".../images/"
+}, {
+	artist: "",
+	genre: "R&B",
+	song: "",
+	image: ".../images/"
+}, {
+	artist: "",
+	genre: "R&B",
+	song: "",
+	image: ".../images/"
+}, {
+	artist: "",
+	genre: "R&B",
+	song: "",
+	image: ".../images/"
+}, {
+	artist: "",
+	genre: "R&B",
+	song: "",
+	image: ".../images/"
+}, {
+	artist: "",
+	genre: "R&B",
+	song: "",
+	image: ".../images/"
+}, {
+	artist: "",
+	genre: "Country",
+	song: "",
+	image: ".../images/"
+}, {
+	artist: "",
+	genre: "Country",
+	song: "",
+	image: ".../images/"
+}, {
+	artist: "",
+	genre: "Country",
+	song: "",
+	image: ".../images/"
+}, {
+	artist: "",
+	genre: "Country",
+	song: "",
+	image: ".../images/"
+}, {
+	artist: "",
+	genre: "Country",
+	song: "",
+	image: ".../images/"
 }]
